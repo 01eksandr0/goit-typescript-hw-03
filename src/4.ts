59f5a1bbac294589ea27d0160037fd479a87f938
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-07 15:29:53 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,35 @@
+class Key {
+  signature: number = Math.random();
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): number {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  constructor(protected key: Key) {}
+  public door: boolean = false;
+  public tenants: Person[] = [];
+  comeIn(person: Person) {
+    this.tenants.push(person);
+  }
+  abstract openDoor(key: number | undefined): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: number | undefined): void {
+    if (key === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +38,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
